9aabcb25f96463ea870467ad25384e2130d4ca98
promessed <promessed97@gmail.com>
2020-04-01 09:22:44 +0200

    Integrating jason web tokens

--- src/index.js
+++ src/index.js
@@ -6,10 +6,30 @@ const taskRouter = require('../src/routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
+// app.use((req,res, next)=>{
+// if(req.method === 'GET'){
+// res.send('GET requests are disable')
+// }else{
+//   next()
+// }
 
+// })
+// app.use((req, res,next)=>{
+// res.status(503).send('our server is under maintenance')
+// })
 app.use(express.json())
 app.use(userRouter,taskRouter)
 
 app.listen(port,()=>{
   console.log('the server is up on '+port)
 })
+
+const jwt = require('jsonwebtoken')
+const myFunction = async () =>{
+const token = jwt.sign({_id:'1232ede'},'MyfirstBorn',{expiresIn: '1 day'})
+console.log(token)
+
+const data = jwt.verify(token, 'MyfirstBorn')
+console.log(data)
+}
+myFunction()
