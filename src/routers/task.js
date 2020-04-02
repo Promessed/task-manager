da5d15bf4314e21b11c24e41ce5ef9828ae1815a
promessed <promessed97@gmail.com>
2020-04-02 12:20:07 +0200

    Setup pagination for tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,6 +19,7 @@ router.post('/tasks', auth , async (req , res)=>{
   })
   
   //GET /tasks?completed=true
+  //GET /tasks?limit=10&skip=20
   router.get('/tasks',auth, async(req,res)=>{
     const match = {}
     if(req.query.completed){
@@ -27,7 +28,11 @@ router.post('/tasks', auth , async (req , res)=>{
     try {
       await req.user.populate({
         path: 'tasks',
-        match
+        match,
+        options: {
+          limit : parseInt(req.query.limit),
+          skip: parseInt(req.query.skip)
+        }
       }).execPopulate()
       res.send(req.user.tasks)
     } catch (error) {
