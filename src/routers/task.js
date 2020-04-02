29a906472246ed592efd7bb4545a7e48b986b332
promessed <promessed97@gmail.com>
2020-04-02 14:41:03 +0200

    Setup task sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -22,6 +22,11 @@ router.post('/tasks', auth , async (req , res)=>{
   //GET /tasks?limit=10&skip=20
   router.get('/tasks',auth, async(req,res)=>{
     const match = {}
+    const sort = {}
+    if(req.query.sortBy){
+      const parts = req.query.sortBy.split(':')
+      sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
     if(req.query.completed){
     match.completed = req.query.completed === 'true'
     }
@@ -31,7 +36,8 @@ router.post('/tasks', auth , async (req , res)=>{
         match,
         options: {
           limit : parseInt(req.query.limit),
-          skip: parseInt(req.query.skip)
+          skip: parseInt(req.query.skip),
+          sort
         }
       }).execPopulate()
       res.send(req.user.tasks)
