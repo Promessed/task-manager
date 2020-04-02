8029ad7ce1c11d51bfa421e4822cae3dd05d338c
promessed <promessed97@gmail.com>
2020-04-02 11:42:22 +0200

    Filter tasks by 'completed'

--- src/index.js
+++ src/index.js
@@ -6,32 +6,9 @@ const taskRouter = require('../src/routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
-// app.use((req,res, next)=>{
-// if(req.method === 'GET'){
-// res.send('GET requests are disable')
-// }else{
-//   next()
-// }
-
-// })
-// app.use((req, res,next)=>{
-// res.status(503).send('our server is under maintenance')
-// })
 app.use(express.json())
 app.use(userRouter,taskRouter)
 
 app.listen(port,()=>{
   console.log('the server is up on '+port)
 })
-
-const User = require('./models/user')
-
-const main = async () => {
-// const task = await Task.findById('5e84a949808d530cb82b0049')
-// await task.populate('owner').execPopulate()
-// console.log(task.owner)
-const user = await User.findById('5e84a933808d530cb82b0047')
-await user.populate('tasks').execPopulate()
-console.log(user.tasks)
-}
-main()
--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt  = require('jsonwebtoken')
+const Task = require('./task')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -48,8 +49,11 @@ const userSchema = new mongoose.Schema({
             type:String,
             required:true
         }
-    }]
+    }],
+    },{
+        timestamps: true
     })
+    
     userSchema.virtual('tasks',{
         ref: 'Task',
         localField: '_id',
@@ -93,6 +97,13 @@ const userSchema = new mongoose.Schema({
       }
 
       next()
+    })
+
+    // Delete tasks of a user who is removed
+    userSchema.pre('remove', async function (next){
+     const user = this
+     await Task.deleteMany({owner: user._id})
+     next()
     })
 
 const User = mongoose.model('User',userSchema)
